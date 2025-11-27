604422aa733a052eed6613fe71d3b7e568abe1b6
davefavismortlock <d.favismortlock@gmail.com>
2025-11-27 10:35:03 +0000

    More re coast tracing

--- src/cme.h
+++ src/cme.h
@@ -752,7 +752,7 @@ double const MIN_SLUMP_VOLUME = 0.001;  // 1 mm average depth over 1 m² cell
 double const SLUMP_REDISTRIBUTION_FRACTION = 0.5;
 
 
-string const PROGRAM_NAME = "Coastal Modelling Environment (CoastalME) version 1.4.1 (26 Nov 2025)";
+string const PROGRAM_NAME = "Coastal Modelling Environment (CoastalME) version 1.4.1 (27 Nov 2025)";
 string const PROGRAM_NAME_SHORT = "CME";
 string const CME_INI = "cme.ini";
 string const CME_YAML = "cme.yaml";
